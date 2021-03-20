4e9b4df784615a701ecbe3ccdaab20f8cfbb7c54
radio <474775913@qq.com>
2021-03-20 23:13:49 +0800

    返回JSON数据

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 const http = require('http')
+let HeaderContentType = {
+  HTML: 'text/html',
+  JSON: 'application/json; charset=utf-8'
+}
 
 const server = http.createServer((request, response) => {
-  response.write('hello')
+  console.log(request.url)
+  let data = {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  }
+  let resData = JSON.stringify(data)
+  response.writeHead('200', {
+    'Content-Type': HeaderContentType.JSON
+  })
+  response.write(resData)
   response.end()
 })
 
