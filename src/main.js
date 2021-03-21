632ffb5b9f6f649918410e08d4a4868fd9a1806f
radio <474775913@qq.com>
2021-03-21 14:30:00 +0800

    引入express框架

--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
-const http = require('http')
-let HeaderContentType = {
-  HTML: 'text/html',
-  JSON: 'application/json; charset=utf-8'
-}
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((request, response) => {
-  let data = {
+app.listen(port, () => {
+  console.log('^_^, 服务已经启动')
+})
+const data = [
+  {
     id: 1,
     title: '关山月',
     content: '明月出天山，苍茫云海间'
-  }
-  let resData = JSON.stringify(data)
-  response.writeHead('200', {
-    'Content-Type': HeaderContentType.JSON
-  })
-  response.write(resData)
-  response.end()
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝岭，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红似火，春来江水绿如蓝'
+  },
+]
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(data))
 })
 
-server.listen('3000', () => {
-  console.log('服务已经启动了...')
+app.get('/posts/:postId', (req, res) => {
+  const { postId } = req.params // 获取请求参数
+  const posts = data.filter((item) => {
+    return item.id === Number(postId)
+  })
+  res.send(JSON.stringify(posts[0]))
 })
